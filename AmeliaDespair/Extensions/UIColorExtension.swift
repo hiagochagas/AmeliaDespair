a353af28dffb32a26f867138ebd367683ebe574f
Rudigus <rodrigo.matos9@hotmail.com>
2021-03-23 11:56:18 -0300

    adds main menu interface and play button functionality

---
+++ AmeliaDespair/Extensions/UIColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColorExtension.swift
+//  AmeliaDespair
+//
+//  Created by Rodrigo Matos Aguiar on 23/03/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static var labelLightGray: UIColor {
+        return #colorLiteral(red: 0.7294117647, green: 0.7098039216, blue: 0.6705882353, alpha: 1)
+    }
+}
--- AmeliaDespair/Shared/SceneDelegate.swift
+++ AmeliaDespair/Shared/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let gameViewController = GameViewController()
-        window.rootViewController = gameViewController
+        let mainMenuViewController = MainMenuViewController()
+        window.rootViewController = mainMenuViewController
         window.makeKeyAndVisible()
         self.window = window
     }
